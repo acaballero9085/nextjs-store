08d5d222cb452221fb1d49fc9cd854746dbfab4d
Arturo Caballero <acaballero9085@gmail.com>
2022-04-05 23:09:29 -0500

    navbar

--- component/layout/navbar.js
+++ component/layout/navbar.js
@@ -1,7 +1,18 @@
-import styles from './navbar.module.css';
+import styles from "./navbar.module.css";
 
 const Navbar = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <span>menuisier</span>
+
+      <div className={styles.navItems}>
+        <button>store</button>
+        <button>about</button>
+      </div>
+
+      <div className={styles.avatar}></div>
+    </div>
+  );
 };
 
 export default Navbar;
